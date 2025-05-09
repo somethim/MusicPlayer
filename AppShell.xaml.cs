164ba4221d1be781d366c650c3286ab2d2fe9bd4
Arbi Kullakshi <arbikullakshi0@gmail.com>
2025-05-09 11:29:38 -0700

    init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicPlayer;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicPlayer;
+
+public partial class MainPage
+{
+    private int _count;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        _count++;
+
+        CounterBtn.Text = _count == 1 ? $"Clicked {_count} time" : $"Clicked {_count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
