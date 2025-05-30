3d959c1e75b4bf825a0e7bd7d17eebb7d6142dcb
dorademiri <ddemiri@uet.edu.al>
2025-05-30 13:16:50 +0200

    chore: migrate to WindowsForms

---
+++ Auth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sign_up_button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,10 @@
+namespace MusicPlayer
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
